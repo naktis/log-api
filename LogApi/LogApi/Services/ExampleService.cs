8fb0b899e0da16feaa17d2b5323addb2c0fc14f5
Augustina <37456430+Naktis@users.noreply.github.com>
2021-09-27 12:47:48 +0300

    Add Get methods

--- LogApi/LogApi/Services/ExampleService.cs
+++ LogApi/LogApi/Services/ExampleService.cs
@@ -1,4 +1,5 @@
 ﻿using LogApi.Dto;
+using System.Collections.Generic;
 
 namespace LogApi.Services
 {
@@ -7,6 +8,24 @@ namespace LogApi.Services
         public override void Create(LogDto log) { }
 
         public override bool ReadsAllowed()
+        {
+            return true;
+        }
+
+        public override LogDto Read(int id)
+        {
+            return new LogDto{ Level = "divine" };
+        }
+
+        public override IEnumerable<LogDto> ReadAll()
+        {
+            return new List<LogDto>() { 
+                new LogDto { Level = "divine" },
+                new LogDto { Level = "info" }
+            };
+        }
+
+        public override bool Exists(int id)
         {
             return true;
         }
